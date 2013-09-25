f5ce5a43e252579fc5bf953a5fe6938274b867ec
Patricia Rosembluth <patty_rosembluth@hotmail.com>
2013-09-25 14:30:31 -0400

    Calcular descuento para cantidad de libros diferentes

--- src/main/java/KataPotter.java
+++ src/main/java/KataPotter.java
@@ -1,9 +1,26 @@
 
 public class KataPotter {
 
-	public int CalcularPrecio(int cantidad) 
+	public int CalcularPrecioSinDescuento(int cantidad) 
 	{
 		return 8*cantidad;
+	}
+	
+	public double CalcularPrecioConDescuento(String lista) 
+	{
+		if(lista=="1 2")
+			return 8 * 2 * 0.95;
+		else
+			if(lista=="1 3 5")
+				return 8 * 3 * 0.9;
+			else
+				if(lista=="1 2 3 4")
+					return 8 * 4 * 0.8;
+				else
+					if(lista=="1 2 3 4 5")
+						return 8 * 5 * 0.75;
+					else
+						return 0;
 	}
 
 }
--- src/test/java/testKataPotter.java
+++ src/test/java/testKataPotter.java
@@ -22,9 +22,19 @@ public class testKataPotter
 	public void testBasico() 
 	{
 		int cantidad=3;
-		assertEquals(0,kp.CalcularPrecio(0));
-		assertEquals(8,kp.CalcularPrecio(1));
-		assertEquals(8*cantidad ,kp.CalcularPrecio(cantidad));
+		assertEquals(0,kp.CalcularPrecioSinDescuento(0));
+		assertEquals(8,kp.CalcularPrecioSinDescuento(1));
+		assertEquals(8*cantidad ,kp.CalcularPrecioSinDescuento(cantidad));
 	}
 
+	@Test
+	public void testDecuentosSimples()
+	{
+		 assertEquals(15,2,kp.CalcularPrecioConDescuento("1 2"));
+		 assertEquals(21,6,kp.CalcularPrecioConDescuento("1 3 5"));
+		 assertEquals(25,6,kp.CalcularPrecioConDescuento("1 2 3 4"));
+		 assertEquals(30,0,kp.CalcularPrecioConDescuento("1 2 3 4 5"));
+	}
+
+	
 }
