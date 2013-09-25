eda16f89e830a7fc13c8fa23d5e7ce6e29b2b362
Patricia Rosembluth <patty_rosembluth@hotmail.com>
2013-09-25 14:41:54 -0400

    Codigo refactorizado

--- src/main/java/KataPotter.java
+++ src/main/java/KataPotter.java
@@ -1,6 +1,14 @@
 
 public class KataPotter {
 
+	private static final String _lISTADO_1 = "1 2";
+	private static final String _lISTADO_2 = "1 3 5";
+	private static final String _lISTADO_3 = "1 2 3 4";
+	private static final String _lISTADO_4 = "1 2 3 4 5";
+	private static final double _DESCUENTO_1=15.2;
+	private static final double _DESCUENTO_2=21.6;
+	private static final double _DESCUENTO_3=25.6;
+	private static final double _DESCUENTO_4=30.0;
 	public int CalcularPrecioSinDescuento(int cantidad) 
 	{
 		return 8*cantidad;
@@ -8,17 +16,17 @@ public class KataPotter {
 	
 	public double CalcularPrecioConDescuento(String lista) 
 	{
-		if(lista=="1 2")
-			return 8 * 2 * 0.95;
+		if(lista==_lISTADO_1)
+			return _DESCUENTO_1;
 		else
-			if(lista=="1 3 5")
-				return 8 * 3 * 0.9;
+			if(lista==_lISTADO_2)
+				return _DESCUENTO_2;
 			else
-				if(lista=="1 2 3 4")
-					return 8 * 4 * 0.8;
+				if(lista==_lISTADO_3)
+					return _DESCUENTO_3;
 				else
-					if(lista=="1 2 3 4 5")
-						return 8 * 5 * 0.75;
+					if(lista==_lISTADO_4)
+						return _DESCUENTO_4;
 					else
 						return 0;
 	}
