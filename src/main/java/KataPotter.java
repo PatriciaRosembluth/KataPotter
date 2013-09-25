3d4476cf8674e2baa27deed80e2714bc0cccfef5
Patricia Rosembluth <patty_rosembluth@hotmail.com>
2013-09-25 13:34:05 -0400

    Precio para 0 libros

---
+++ src/main/java/KataPotter.java
@@ -0,0 +1,12 @@
+
+public class KataPotter {
+
+	public String CalcularPrecio(int numero) 
+	{
+		if(numero==0)
+			return "0";
+		else
+			return "Error";
+	}
+
+}
--- src/test/java/testKataPotter.java
+++ src/test/java/testKataPotter.java
@@ -1,13 +1,23 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
-public class testKataPotter {
+public class testKataPotter 
+{
+	KataPotter kp;
+	
+	@Before
+	public void setUp()
+	{
+		kp=new KataPotter();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNoSeComproNingunLibro() 
+	{
+		assertEquals("0",kp.CalcularPrecio(0));
 	}
 
 }
