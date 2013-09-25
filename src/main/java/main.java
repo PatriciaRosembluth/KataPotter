64a3b194accbe9184146b10ffa7c9f5aafa81271
Patricia Rosembluth <patty_rosembluth@hotmail.com>
2013-09-25 17:04:23 -0400

    Calcular descuentos especiales

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+
+public class main 
+{
+	public static void main(String[] args) 
+	{
+		KataPotter kp = new KataPotter();
+		System.out.println(kp.CalcularPrecioSinDescuento(2));
+		System.out.println(kp.CalcularPrecioConDescuento("1 2"));	
+	}
+
+}
